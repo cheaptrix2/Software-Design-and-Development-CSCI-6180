9a056379ac1ef67856a1269c4d0ad9a4b13b3d93
Joseph May <josephmay2000@gmail.com>
2023-11-21 11:05:08 -0600

    CSS connected to html templates, using ML class for classification

--- SoftEngProj/application.py
+++ SoftEngProj/application.py
@@ -4,6 +4,13 @@ import mysql.connector
 import os
 from werkzeug.utils import secure_filename
 import csv
+import sys
+
+bigfolder_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(bigfolder_path)
+from ML.ECGClass import ECGClassifier
+
+
 MAX_CLIENTS = 100
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads'
@@ -18,6 +25,11 @@ db = mysql.connector.connect(
     database="HEARTHEALTH"
 )
 
+#Initalizing NN model
+# This code block can be placed anywhere, like whenever server starts
+ecg_classifier = ECGClassifier() # Making an object
+ecg_classifier.load_model() # Making the model
+
 cursor = db.cursor()
 #cursor.execute("CREATE TABLE IF NOT EXISTS users (id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(255), password VARCHAR(255))")
 
@@ -31,16 +43,25 @@ def query_database(username, password):
     return result
 
 def process_csv(file_path):
+    
+    ecg_classifier.load_data(file_path) # Load data 
+    ecg_classifier.predict() # Perform prediction within the object
+    results = ecg_classifier.print_predictions() # returns a list of prediction
     with open(file_path, 'r') as file:
+        #file = 'Test.csv' # A test file without the final label
+        
         reader = csv.reader(file)
-        rows = [row + ['0'] for row in reader]
+        rows = []
+        for row,result in zip(reader,results):
+            rows.append(row + [result]) 
+        #rows = [row + ['0'] for row in reader]
 
     modified_file_path = f"{file_path}_modified.csv"
     with open(modified_file_path, 'w', newline='') as modified_file:
         writer = csv.writer(modified_file)
         writer.writerows(rows)
 
-    return modified_file_path
+    return modified_file_path,results
 
 @socketio.on('connect')
 def handle_connect():
@@ -65,7 +86,6 @@ def login():
         username = request.form['username']
         password = request.form['password']
         user = query_database(username, password)
-        print('User is:', user)
         if user is not None:
             print(f"Redirecting to results for {username}")
             session['username'] = username
@@ -91,12 +111,12 @@ def results():
             if uploaded_file and uploaded_file.filename.endswith('.csv'):
                 filename = secure_filename(uploaded_file.filename)
                 file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-
+                
                 # Save the uploaded file
-                uploaded_file.save(file_path)
+                #uploaded_file.save(file_path)
 
                 # Process the CSV file and get the modified file path
-                modified_file_path = process_csv(file_path)
+                modified_file_path,results = process_csv(file_path)
 
                 # Set up response to allow file download
                 return send_file(
@@ -105,10 +125,14 @@ def results():
                     download_name=f'modified_{filename}',
                     mimetype='text/csv'
                 )
+                display_results()
 
         return render_template('results.html', username=username, uploaded_filename=uploaded_filename)
     
     return redirect(url_for('login'))
+
+def display_results():
+    print('Show results 1. result 2. results etc in that hidden div')
 
 @app.route('/')
 def about():
